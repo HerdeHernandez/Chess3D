b13aa5d351694c6112312819b8e1811534982b19
HerdeHernandez <94528237+HerdeHernandez@users.noreply.github.com>
2022-02-08 08:24:51 +0800

    update

---
+++ Assets/Script/rotateBoard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateBoard : MonoBehaviour
+{
+
+    float speed = 10f;
+    public void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * speed;
+       // float rotY = Input.GetAxis("Mouse Y") * speed;
+
+        transform.Rotate(Vector3.down,  rotX);
+       // transform.Rotate(Vector3.right,  rotY);
+    }
+
+    private void Update()
+    {
+       
+
+
+    }
+
+}
